85bfbbc2d7223969110351f8907a91a31dc4675b
Anna Artemova <annartmva@gmail.com>
2024-01-26 16:14:38 +0400

    Fix deployment error

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/frontend-mentor-frontend-quiz-app/",
   plugins: [react()],
 });
